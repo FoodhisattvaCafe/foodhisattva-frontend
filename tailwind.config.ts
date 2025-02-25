d1dab039cb739d9a13b2c1f6a7bdfbd3388a2ee5
kouushik <64321552+kouushik@users.noreply.github.com>
2025-02-25 16:08:03 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,33 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': '375px',     // Extra small devices
+      'sm': '640px',     // Small devices
+      'md': '768px',     // Medium devices
+      'lg': '1024px',    // Large devices
+      'xl': '1280px',    // Extra large devices
+      '2xl': '1536px',   // 2X large devices
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'accent-primary': '#94C973',
+        'accent-dark': '#7bab5a',
+        'accent-light': '#b6e095',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%', // Ensures typography doesn't have fixed width
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+  darkMode: 'class',
 } satisfies Config;
