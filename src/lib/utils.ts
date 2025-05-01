1bc7f6b30718e031993755f5d60ee7962c3a64f2
NarsimhaLakavath <narsimhalakavath166@gmail.com>
2025-04-30 23:02:36 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * @function cn
+ * @description
+ * Utility function to conditionally merge Tailwind CSS class names.
+ * Uses `clsx` to conditionally combine class names and `tailwind-merge` to remove conflicts.
+ *
+ * @param {...ClassValue[]} inputs - Array of class values to be merged.
+ * @returns {string} A single string of valid, merged class names.
+ *
+ * @example
+ * const buttonClass = cn("btn", isPrimary && "btn-primary", "text-lg");
+ * // Output: "btn btn-primary text-lg" (depending on condition)
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
